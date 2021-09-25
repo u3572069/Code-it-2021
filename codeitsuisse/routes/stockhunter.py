fd705e15273a366e007cbf0be1a1478abe5fe523
u3572069 <u3572079@connect.hku.hk>
2021-09-26 07:28:17 +0800

    Update stockhunter.py

--- codeitsuisse/routes/stockhunter.py
+++ codeitsuisse/routes/stockhunter.py
@@ -55,9 +55,9 @@ import sys
 def return_index(rect_diff_y, rect_diff_x, verticalStepper, horizontalStepper, griddepth, gridkey, target_x, target_y):
     grid = [[0 for x in range(rect_diff_y+1)] for x in range(rect_diff_x+1)]
     for i in range(len(grid)):
-        grid[i][0] = i * horizontalStepper
+        grid[i][0] = i * verticalStepper
     for j in range(len(grid[0])):
-        grid[0][j] = j * verticalStepper
+        grid[0][j] = j * horizontalStepper
     for i in range(1,len(grid)):
         for j in range(1,len(grid[0])):
                 grid[i][j] = (grid[i-1][j]+griddepth)%gridkey * (grid[i][j-1]+griddepth)%gridkey
